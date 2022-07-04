3daae68015fc9259d8920f260dc62a66722bfdd2
Arno <koskarno2@gmail.com>
2022-07-04 12:51:38 +0300

    changes

--- public/js/edasimyyjad.js
+++ public/js/edasimyyjad.js
@@ -1,19 +1,25 @@
-let slideIndex = 0;
-showSlides();
+var slideshows = document.querySelectorAll('[data-component="slideshow"]');
 
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+// Apply to all slideshows that you define with the markup wrote
+slideshows.forEach(initSlideShow);
+
+function initSlideShow(slideshow) {
+
+  var slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`); // Get an array of slides
+
+  var index = 0, time = 3000;
+  slides[index].classList.add('active');
+
+  setInterval( () => {
+    slides[index].classList.remove('active');
+
+    //Go over each slide incrementing the index
+    index++;
+
+    // If you go over all slides, restart the index to show the first slide and start again
+    if (index === slides.length) index = 0;
+
+    slides[index].classList.add('active');
+
+  }, time);
 }
