bfb9033e0e605886a0035157a856db940896203d
siily <arnokosk15@gmail.com>
2023-10-27 13:37:50 +0300

    deleted diploms and fixed navigation in redasimyyjad

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,3 @@
-let lang = navigator.language.slice(0, 2)
-
-if (lang === 'en') {
-    document.querySelectorAll('.second-lanuge').forEach(elem => elem.style.display = 'none');
-} else {
-    document.querySelectorAll('.english').forEach(elem => elem.style.display = 'none');
-}
 
 var navList = document.getElementById("nav-lists");
 function Show() {
