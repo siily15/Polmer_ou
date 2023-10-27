d68128373392d4808fd2aed5da750b62c45272d1
siily <arnokosk15@gmail.com>
2023-10-27 13:28:41 +0300

    removed diploms and fixed navigation

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,11 @@
+let lang = navigator.language.slice(0, 2)
+
+if (lang === 'en') {
+    document.querySelectorAll('.second-lanuge').forEach(elem => elem.style.display = 'none');
+} else {
+    document.querySelectorAll('.english').forEach(elem => elem.style.display = 'none');
+}
+
 var navList = document.getElementById("nav-lists");
 function Show() {
 navList.classList.add("_Menus-show");
